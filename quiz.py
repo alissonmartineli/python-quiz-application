1e2879591ef0afc5ad474e4f1b1452c64eb19643
Alisson Martineli <martineli.alisson@gmail.com>
2022-06-28 20:33:50 -0300

    feat: add variety to quiz

--- quiz.py
+++ quiz.py
@@ -1,5 +1,7 @@
+import random
 from string import ascii_lowercase
 
+NUM_QUESTIONS_PER_QUIZ = 5
 QUESTIONS = {
     'When was the first known use of the word \'quiz\'': [
         '1781', '1771', '1871', '1881'
@@ -18,15 +20,20 @@ QUESTIONS = {
     ],
 }
 
+num_questions = min(NUM_QUESTIONS_PER_QUIZ, len(QUESTIONS))
+questions = random.sample(list(QUESTIONS.items()), k=num_questions)
+
 num_correct = 0
 
-for num, (question, alternatives) in enumerate(QUESTIONS.items(), start=1):
+for num, (question, alternatives) in enumerate(questions, start=1):
     print(f'\nQuestion {num}:')
     print(f'{question}?')
 
     correct_answer = alternatives[0]
 
-    labeled_alternatives = dict(zip(ascii_lowercase, sorted(alternatives)))
+    labeled_alternatives = dict(
+        zip(ascii_lowercase, random.sample(alternatives, k=len(alternatives)))
+    )
     for label, alternative in labeled_alternatives.items():
         print(f'  {label}) {alternative}')
 
