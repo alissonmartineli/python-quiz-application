17c1ddebf7ef51fdeb29f5818ae5a50aafcef0eb
Alisson Martineli <martineli.alisson@gmail.com>
2022-06-28 21:07:04 -0300

    refactor: separate data into its own file

--- quiz.py
+++ quiz.py
@@ -1,24 +1,14 @@
 import random
 from string import ascii_lowercase
+import pathlib
+try:
+    import tomllib
+except ModuleNotFoundError:
+    import tomli as tomllib
 
 NUM_QUESTIONS_PER_QUIZ = 5
-QUESTIONS = {
-    'When was the first known use of the word \'quiz\'': [
-        '1781', '1771', '1871', '1881'
-    ],
-    'Which built-in function can get information from the user': [
-        'input', 'get', 'print', 'write'
-    ],
-    'Which keyword do you use to loop over a given list of elements': [
-        'for', 'while', 'each', 'loop'
-    ],
-    'What\'s the purpose of the built-in zip() function': [
-        'To iterate over two or more sequences at the same time',
-        'To combine several strings into one',
-        'To compress several files into one archive',
-        'To get information from the user',
-    ],
-}
+QUESTIONS_PATH = pathlib.Path(__file__).parent / "questions.toml"
+QUESTIONS = tomllib.loads(QUESTIONS_PATH.read_text())
 
 
 def run_quiz():
